db641b3d88a17119b70ff504334123d33e813ca6
Samuel Olaide Okediji <samuelolaideokediji@gmail.com>
2026-07-01 14:21:40 +0100

    chore: update Next.js landing page for Vercel

---
+++ .next/static/IJ-l1WDoelWJIHdGLF1Ta/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/401-1acf4f2234285d6b.js","static/chunks/pages/index-1ac1bf4b9f5d41c9.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
